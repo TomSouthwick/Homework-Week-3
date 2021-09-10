22dee568ea505f45da5e967b2ac743c74a75652c
Tom Southwick <thomas.h.southwick@gmail.com>
2021-09-10 15:28:23 +1000

    Homework update

--- Develop/script.js
+++ Develop/script.js
@@ -1,46 +1,54 @@
 // Assignment Code
-const includeNumbersElement = document.getElementById("includeNumbers");
-const includeSymbolsElement = document.getElementById("includeSymbols");
-const includeUppercaseElement = document.getElementById("includeUppercase");
-const characterAmountRange = document.getElementById("characterAmountRange");
-const characterAmountNumber = document.getElementById("characterAmountNumber");
-const form = document.getElementById("passwordGeneratorForm")
-const password = document.getElementById("password")
+var includeNumbersElement = document.getElementById("includeNumbers");
+var includeSymbolsElement = document.getElementById("includeSymbols");
+var includeUppercaseElement = document.getElementById("includeUppercase");
+var characterAmountRange = document.getElementById("characterAmountRange");
+var characterAmountNumber = document.getElementById("characterAmountNumber");
+var btnObject = document.getElementById("generate")
+var password = document.getElementById("password")
 
-const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
-const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
-const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
-const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)
+var UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+var LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+var NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+var SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)
 ).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126)
 )
 
 characterAmountNumber.addEventListener("input", syncCharacterAmount);
 characterAmountRange.addEventListener("input", syncCharacterAmount);
 
-form.addEventListener("submit", e => {
+
+// Event listener when button gets clicked
+btnObject.addEventListener("click", e => {
   e.preventDefault()
-  const characterAmount = characterAmount.value
-  const includeUppercase = includeUppercaseElement.checked
-  const includeSymbols = includeSymbolsElement.checked
-  const includeNumbers = includeNumbersElement.checked
-  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
-  password.addEventListener = password
+  // Fetch all the checkboxes details that user ticked
+  var characterAmount = characterAmountNumber.value
+  var includeUppercase = includeUppercaseElement.checked
+  var includeSymbols = includeSymbolsElement.checked
+  var includeNumbers = includeNumbersElement.checked
+  var passwordSecure = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+
+  // show the secure password in the e text area
+  password.innerHTML = passwordSecure
 })
+
+// Function to generate a secure password
 function generatePassword(characterAmount, includeUppercase,includeNumbers, includeSymbols) {
   let charCodes = LOWERCASE_CHAR_CODES
   if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
   if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
   if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
-  const passwordCharacters  = []
+  var passwordCharacters  = []
   for (let i = 0; i < characterAmount; i++) {
-    const characterCode = charCodes[Math.floor(math.random() * charCodes.length)]
+    var characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
     passwordCharacters.push(String.fromCharCode(characterCode))
   }
+  //alert(passwordCharacters);
 return passwordCharacters.join("")
 }
 
 function arrayFromLowToHigh(low , high) {
-  const array = []
+  var array = []
   for (let i = low; i <= high; i++) {
     array.push(i)
   }
@@ -48,7 +56,7 @@ function arrayFromLowToHigh(low , high) {
 }
 
 function syncCharacterAmount(e) {
-  const value = e.target.value
+  var value = e.target.value
   characterAmountNumber.value = value
   characterAmountRange.value = value
 }
